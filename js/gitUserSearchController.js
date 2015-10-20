f9f618eea8eafce874c2597ec386077934bec21a
Lucy Fang <lucyfang2@gmail.com>
2015-10-20 16:24:30 +0100

    end of version 1 about to do bullet points

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,37 +1,14 @@
-githubUserSearch.controller('GitUserSearchController', [function() {
+githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
 
+  var searchResource = $resource('https://api.github.com/search/users');
   var self = this;
 
   self.doSearch = function (){
-    self.searchResult = {
-      items: [
-        {
-          "login": "tansaku",
-          "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-          "html_url": "https://github.com/tansaku"
-        },
-        {
-          "login": "stephenlloyd",
-          "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-          "html_url": "https://github.com/stephenlloyd"
-        }
-      ]
-    };
+    self.searchResult = searchResource.get(
+      { q: self.searchTerm, access_token: 'yourtoken' }
+    );
   };
 
-  // self.searchResult = {
-  //   "items": [
-  //     {
-  //       "login": "tansaku",
-  //       "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-  //       "html_url": "https://github.com/tansaku"
-  //     },
-  //     {
-  //       "login": "stephenlloyd",
-  //       "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-  //       "html_url": "https://github.com/stephenlloyd"
-  //     }
-  //   ]
-  // };
+
 
 }]);
--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -12,34 +12,41 @@ describe('GitUserSearchController', function() {
     expect(ctrl.searchTerm).toBeUndefined();
   });
 
-});
+  describe('when searching for a user', function() {
 
-describe('when searching for a user', function() {
-  beforeEach(module('GitUserSearch'));
+    var httpBackend;
+    beforeEach(inject(function($httpBackend) {
+        httpBackend = $httpBackend
+        httpBackend
+          .expectGET("https://api.github.com/search/users?access_token=yourtoken&q=hello")
+          .respond(
+            { items: items }
+          );
+    }));
 
-  var ctrl;
+    afterEach(function() {
+      httpBackend.verifyNoOutstandingExpectation();
+      httpBackend.verifyNoOutstandingRequest();
+    });
 
-  beforeEach(inject(function($controller) {
-    ctrl = $controller('GitUserSearchController');
-  }));
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      },
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ];
 
-  var items = [
-    {
-      "login": "tansaku",
-      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-      "html_url": "https://github.com/tansaku"
-    },
-    {
-      "login": "stephenlloyd",
-      "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-      "html_url": "https://github.com/stephenlloyd"
-    }
-  ];
-
-  it('displays search results', function() {
-    // ctrl.searchTerm = 'hello';
-    ctrl.doSearch();
-    expect(ctrl.searchResult.items).toEqual(items);
+    it('displays search results', function() {
+      ctrl.searchTerm = 'hello';
+      ctrl.doSearch();
+      httpBackend.flush();
+      expect(ctrl.searchResult.items).toEqual(items);
+    });
   });
-
 });
